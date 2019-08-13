fc3c9920afd7456f01695b4a5c345071d9c7ddd8
Nicholas Byfleet <nick.byfleet@heyday.co.nz>
2019-08-13 16:09:16 +1200

    Upgrading package to silverstripe 4.x

--- code/HoneyPotSpamProtector.php
+++ code/HoneyPotSpamProtector.php
@@ -1,23 +1,27 @@
 <?php
 
-/**
- * @package honeypot
- */
+namespace Heyday\SilverStripe\HoneyPot;
+
+use SilverStripe\SpamProtection\SpamProtector;
 
 class HoneyPotSpamProtector implements SpamProtector {
 
-	/**
-	 * Returns the {@link HoneyPotField} associated with this protector
-	 *
-	 * @return HoneyPotField
-	 */
+    /**
+     * Returns the {@link HoneyPotField} associated with this protector
+     *
+     * @param null $name
+     * @param null $title
+     * @param null $value
+     * @return HoneyPotField
+     */
 	public function getFormField($name = null, $title = null, $value = null) {
 		return new HoneyPotField($name, $title, $value);
 	}
 
-	/**
-	 * Not used by HoneyPotSpamProtector
-	 */
+    /**
+     * Not used by HoneyPotSpamProtector
+     * @param $fieldMapping
+     */
 	public function setFieldMapping($fieldMapping) {}
 
 }
